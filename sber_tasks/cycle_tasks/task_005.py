fe91c01204c6c4bf02213f72e6fa983e096229c4
Herring Anthony <29326498+antoshick@users.noreply.github.com>
2023-01-19 18:27:27 +0300

    Update task_005.py

--- sber_tasks/cycle_tasks/task_005.py
+++ sber_tasks/cycle_tasks/task_005.py
@@ -19,3 +19,21 @@
          21
          1
 """
+n = int(input('Введите число ступеней меньше 10: '))
+if n > 9:
+    print('Ошибка, количество ступеней должно быть меньше 10')
+else:
+    for i in range(1,n+1):
+        step=' '*(n-i)
+        for j in range(1,i+1):
+            step+= str(j)
+        for j in range(i-1,0,-1):
+            step+= str(j)
+        print(step)
+    for i in range(n-1,0,-1):
+        step=' '*(n-i)
+        for j in range(1,i+1):
+            step += str(j)
+        for j in range(i-1,0,-1):
+            step += str(j)
+        print(step)
